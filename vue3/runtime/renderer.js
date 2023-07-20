54ca9bccbdbfaa5daa8dbc34fc82188ca0d83a2d
zengxinbin <xinbinzeng@xyz10.com>
2023-07-20 18:53:27 +0800

    feat:patch props

--- vue3/runtime/renderer.js
+++ vue3/runtime/renderer.js
@@ -5,6 +5,9 @@ const vnode = {
   type: 'div',
   props: {
     id: 'foo',
+    onclick: () => {
+      alert('clicked')
+    },
   },
   children: [
     {
@@ -15,7 +18,7 @@ const vnode = {
 }
 
 function createRenderer(options) {
-  const { createElement, setElementText, insert } = options
+  const { createElement, setElementText, insert, patchProps } = options
   function render(vnode, container) {
     if (vnode) {
       //patch
@@ -23,13 +26,13 @@ function createRenderer(options) {
     } else {
       if (container._vnode) {
         //delete
-        container.innerHTML = ''
+        unmount(container._vnode)
       }
     }
     container._vnode = vnode
   }
   function mountElement(vnode, container) {
-    const el = createElement(vnode.type)
+    const el = (vnode.el = createElement(vnode.type))
     if (typeof vnode.children === 'string') {
       setElementText(el, vnode.children)
     } else if (Array.isArray(vnode.children)) {
@@ -39,10 +42,15 @@ function createRenderer(options) {
     }
     if (vnode.props) {
       for (const key in vnode.props) {
-        el.setAttribute(key, vnode.props[key])
+        patchProps(key, el, null, vnode.props[key])
       }
     }
     insert(el, container)
+  }
+  function patchElement(n1, n2) {
+    //todo
+    const el = n1.el 
+    el.addEventListener('click', n2.props.onclick)
   }
   /**
    * @param n1 - 旧的元素
@@ -50,10 +58,26 @@ function createRenderer(options) {
    * @param container - 挂载的具体位置
    */
   function patch(n1, n2, container) {
-    if (!n1) {
-      mountElement(n2, container)
+    if (n1 && n1.type !== n2.type) {
+      unmount(n1)
+      n1 = null
+    }
+    const { type } = n2
+    if (typeof type === 'string') {
+      if (!n1) {
+        mountElement(n2, container)
+      } else {
+        patchElement(n1, n2)
+      }
+    } else if (typeof type === 'object') {
+      //组件
     } else {
-      //todo
+    }
+  }
+  function unmount(vnode) {
+    const parent = vnode.el.parentNode
+    if (parent) {
+      parent.removeChild(vnode.el)
     }
   }
   return { render }
@@ -68,6 +92,66 @@ const renderer = createRenderer({
   insert(el, container) {
     container.appendChild(el)
   },
+  patchProps(key, el, preVal, nextVal) {
+    if (/^on/.test(key)) {
+      let invokers ={} //缓存绑定的事件
+      el._vei = invokers
+      let invoker = invokers[key]
+      const name = key.slice(2).toLowerCase()
+      if (nextVal) {
+        if (!invoker) {
+          invoker = el._vei[key] = (e) => {
+            invoker.value(e)
+          }
+          invoker.value = nextVal
+          el.addEventListener(name, invoker)
+        } else {
+          invoker.value = nextVal
+        }
+      } else if (invoker) {
+        el.removeEventListener(name, invoker)
+      }
+    } else if (key === 'class') {
+    } else {
+      if (key in el) {
+        if (nextVal === '') {
+          el[key] = true
+        } else {
+          el[key] = nextVal
+        }
+      } else {
+        el.setAttribute(key, nextVal)
+      }
+    }
+  },
 })
 
-renderer.render(vnode, document.getElementById('app'))
+// renderer.render(vnode, document.getElementById('app'))
+
+const bol = ref(false)
+
+effect(() => {
+  const vnode = {
+    type: 'div',
+    props: bol.value
+      ? {
+          onclick: () => {
+            console.log('click parent')
+          },
+        }
+      : {},
+    children: [
+      {
+        type: 'p',
+        props: {
+          onclick: () => {
+            console.log(1)
+            bol.value = true
+          },
+        },
+        children:'text'
+      },
+    ],
+  }
+renderer.render(vnode, document.querySelector('#app'))
+})
