aac66aad9d0fd1b518089394d675ac126c928b74
zxb.zeng <zxb.zeng@shunwang.com>
2025-04-17 17:28:11 +0800

    feat:init

--- react/my-app/src/store/index.js
+++
@@ -1,7 +0,0 @@
-import create from 'zustand'
-
-export const useCounterStore = create((set) => ({
-  count: 0,
-  increment: () => set((state) => ({ count: state.count + 1 })),
-  decrement: () => set((state) => ({ count: state.count - 1 })),
-}))
---
+++ react/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl font-bold underline">123</div>
+    </>
+  );
+}
+
+export default App;
